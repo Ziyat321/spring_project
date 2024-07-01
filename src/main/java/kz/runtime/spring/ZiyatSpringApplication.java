5b7d9514d9bd7fa1d3fc19e4dbd8148cef74fc73
Ziyat <zippyzoom29@gmail.com>
2024-07-01 12:31:57 +0500

    version1

---
+++ src/main/java/kz/runtime/spring/ZiyatSpringApplication.java
@@ -0,0 +1,13 @@
+package kz.runtime.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiyatSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZiyatSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/runtime/spring/ZiyatSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.runtime.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZiyatSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
