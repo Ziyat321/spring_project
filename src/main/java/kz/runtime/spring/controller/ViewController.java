c8ba4fcad70757dc8e0d567d880aa50b28cf1b86
Ziyat <zippyzoom29@gmail.com>
2024-07-17 15:57:44 +0500

    filter checked

--- src/main/java/kz/runtime/spring/controller/ViewController.java
+++ src/main/java/kz/runtime/spring/controller/ViewController.java
@@ -5,13 +5,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping(path = "/view_controller")
 public class ViewController {
 
     @GetMapping(path = "/resource_1")
-    public String firstResource(Model model){
+    public String firstResource(Model model) {
         String message = "Message from ViewController.firstResource";
         Human human = new Human("Jeff", 14);
         model.addAttribute("human", human);
@@ -19,13 +23,35 @@ public class ViewController {
     }
 
     @GetMapping(path = "/resource_2")
-    public String secondResource(Model model){
+    public String secondResource(Model model,
+                                 @RequestParam(name = "age", required = false) String age) {
         Human[] humans = new Human[]{
                 new Human("Bill", 34),
                 new Human("Jeff", 15),
                 new Human("Mark", 48)
         };
-        model.addAttribute("humans", humans);
+        if (age == null || age.isEmpty()) {
+            model.addAttribute("humans", humans);
+            model.addAttribute("age","");
+        } else {
+            try {
+                int ageInt = Integer.parseInt(age);
+                List<Human> humansList = new ArrayList<Human>();
+                for (Human human : humans) {
+                    if (human.getAge() == ageInt) {
+                        humansList.add(human);
+                    }
+                }
+                model.addAttribute("humans", humansList);
+                model.addAttribute("age",age);
+            } catch (
+                    NumberFormatException e) {
+                model.addAttribute("humans", humans);
+                model.addAttribute("age","");
+            }
+        }
+
+
         return "view_resource_2_page";
     }
 
