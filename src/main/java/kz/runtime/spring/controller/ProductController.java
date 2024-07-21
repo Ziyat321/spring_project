03693f94c187b2f39e0cc468e37d020d8184853c
Ziyat <zippyzoom29@gmail.com>
2024-07-21 15:01:23 +0500

    category-product check

--- src/main/java/kz/runtime/spring/controller/ProductController.java
+++ src/main/java/kz/runtime/spring/controller/ProductController.java
@@ -1,6 +1,8 @@
 package kz.runtime.spring.controller;
 
+import kz.runtime.spring.entity.Category;
 import kz.runtime.spring.entity.Product;
+import kz.runtime.spring.repository.CategoryRepository;
 import kz.runtime.spring.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
@@ -8,21 +10,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Controller
-@RequestMapping(path="/product_controller")
 public class ProductController {
 
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @GetMapping(path = "/products")
-    public String allProducts(Model model){
-        List<Product> products = productRepository.findAll();
-        model.addAttribute("products", products);
+    public String allProducts(Model model,
+                              @RequestParam(name = "categoryId", required = false) Long category){
+        if(category != null){
+            Category category1 = categoryRepository.findById(category).orElseThrow();
+            List<Product> products = category1.getProducts();
+            model.addAttribute("products", products);
+        } else {
+            List<Product> products = productRepository.findAll();
+            model.addAttribute("products", products);
+        }
         return "product_resource_1_page";
     }
+
 }
