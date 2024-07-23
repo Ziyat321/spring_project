5efabf62b01e7b545aef03302df038910c5d8d75
Ziyat <zippyzoom29@gmail.com>
2024-07-23 17:46:17 +0500

    added create product & characteristics pages

--- sql/schema_complete_tables.sql
+++ sql/schema_complete_tables.sql
@@ -5,10 +5,18 @@ insert into category(name)
 values ('Processors'), ('Monitors');
 
 insert into product(category_id, name, price, visibility)
-values (4, 'Ryzen-Threadripper-PRO-7995WX', 10000, true),
-       (4, 'Ryzen-Threadripper-7980X', 9000, true),
-       (4, 'EPYC-9654', 8000, true),
-       (5, 'Asus ROG Strix XG43UQ', 1729, true),
-       (5, 'Samsung Odyssey G9', 1593, true),
-       (5, 'Asus TUF Gaming VG28UQL1A', 858, true),
-       (5, 'Dell UltraSharp U3425WE', 1148, true);
+values (1, 'Ryzen-Threadripper-PRO-7995WX', 10000, true),
+       (1, 'Ryzen-Threadripper-7980X', 9000, true),
+       (1, 'EPYC-9654', 8000, true),
+       (2, 'Asus ROG Strix XG43UQ', 1729, true),
+       (2, 'Samsung Odyssey G9', 1593, true),
+       (2, 'Asus TUF Gaming VG28UQL1A', 858, true),
+       (2, 'Dell UltraSharp U3425WE', 1148, true);
+
+insert into characteristic(category_id, name)
+values (1, 'Количество ядер'),
+       (1, 'Количество потоков'),
+       (1, 'Базовая частота (ГГц)'),
+       (1,'Дата выхода'),
+       (2, 'Разрешение'),
+       (2, 'Размер экрана');
--- src/main/java/kz/runtime/spring/controller/ProductController.java
+++ src/main/java/kz/runtime/spring/controller/ProductController.java
@@ -1,18 +1,20 @@
 package kz.runtime.spring.controller;
 
 import kz.runtime.spring.entity.Category;
+import kz.runtime.spring.entity.Characteristic;
+import kz.runtime.spring.entity.CharacteristicDescription;
 import kz.runtime.spring.entity.Product;
 import kz.runtime.spring.repository.CategoryRepository;
+import kz.runtime.spring.repository.CharacteristicDescriptionRepository;
+import kz.runtime.spring.repository.CharacteristicRepository;
 import kz.runtime.spring.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -22,11 +24,15 @@ public class ProductController {
     private ProductRepository productRepository;
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private CharacteristicRepository characteristicRepository;
+    @Autowired
+    private CharacteristicDescriptionRepository characteristicDescriptionRepository;
 
     @GetMapping(path = "/products")
     public String allProducts(Model model,
-                              @RequestParam(name = "categoryId", required = false) Long category){
-        if(category != null){
+                              @RequestParam(name = "categoryId", required = false) Long category) {
+        if (category != null) {
             Category category1 = categoryRepository.findById(category).orElseThrow();
             List<Product> products = category1.getProducts();
             model.addAttribute("products", products);
@@ -37,12 +43,60 @@ public class ProductController {
         return "product_resource_1_page";
     }
 
+    @GetMapping(path = "/category_choice")
+    public String chooseCategory(Model model) {
+        List<Category> categories = categoryRepository.findAll();
+        model.addAttribute("categories", categories);
+        return "category_choice";
+    }
+
     @GetMapping(path = "/create_product")
-    public String createProduct(
-            @RequestParam(name = "category", required = false) String category,
-            @RequestParam(name = "product", required = false) String product
-    ){
+    public String createProduct() {
         return "product_creation";
+    }
+
+    @PostMapping(path = "/create_product")
+    public String fillProduct(
+            Model model,
+            @RequestParam(name = "categoryId", required = false) Long categoryId
+    ) {
+        if (categoryId != null) {
+            Category category = categoryRepository.findById(categoryId).orElseThrow();
+            model.addAttribute("category", category);
+            model.addAttribute("characteristics", category.getCharacteristics());
+        }
+        return "product_creation";
+    }
+
+    @PostMapping(path = "/products")
+    public String saveProduct(Model model,
+                              @RequestParam(name = "categoryId", required = false) Long categoryId,
+                              @RequestParam(name = "product", required = false) String product,
+                              @RequestParam(name = "price", required = false) Integer price,
+                              @RequestParam(name = "description", required = false) String... description) {
+        if (categoryId != null && product != null && price != null && description != null) {
+            Category category = categoryRepository.findById(categoryId).orElseThrow();
+
+            Product newProduct = new Product();
+            newProduct.setName(product);
+            newProduct.setPrice(price);
+            newProduct.setVisibility(true);
+            newProduct.setCategory(category);
+            productRepository.save(newProduct);
+
+
+            for (int i = 0; i < description.length; i++) {
+                Characteristic characteristic = category.getCharacteristics().get(i);
+                CharacteristicDescription characteristicDescription = new CharacteristicDescription();
+                characteristicDescription.setDescription(description[i]);
+                characteristicDescription.setProduct(newProduct);
+                characteristicDescription.setCharacteristic(characteristic);
+                characteristicDescriptionRepository.save(characteristicDescription);
+            }
+        }
+        List<Product> products = productRepository.findAll();
+        model.addAttribute("products", products);
+        return "product_resource_1_page";
     }
 
 }
